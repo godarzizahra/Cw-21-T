3b9bcfd90475565625f42b173b9ae1142ee4bd0b
zahra <z.godarzi75@gmail.com>
2026-01-08 15:26:51 +0330

    first commit

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+export default function useFetch(url: string) {
+	// const [data, setData] = useState([]);
+	const [data, setData] = useState(null);
+	const [error, setError] = useState(null);
+	const [isLoading, setIsLoading] = useState(false);
+	useEffect(() => {
+		setIsLoading(true);
+		fetch(url)
+			.then((res) => res.json())
+			.then((data) => {
+				setData(data);
+				setError(null);
+			})
+			.catch((error) => setError(error))
+			.finally(() => setIsLoading(false));
+	}, [url]);
+	return { data, error, isLoading };
+}
